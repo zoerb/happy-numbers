a5ae19ff9f8175038da4e1f9d44d84b27cd05642
Ryan Zoerb <ryan.zoerb@gmail.com>
2019-07-18 16:32:57 -0500

    General cleanups

--- src/main/scala/com/zoerb/happynumbers/App.scala
+++ src/main/scala/com/zoerb/happynumbers/App.scala
@@ -1,26 +1,28 @@
 package com.zoerb.happynumbers
 
+import scala.util.Try
+
 
 object App {
   def main(args: Array[String]): Unit = {
-    args.length match {
-      case 0 => 
+    args match {
+      case Array() =>
         // Print out all happy numbers between 1 and 1000
-        val allHappyNums = HappyNumbers.computeAllHappyNumbers(1, 1000)
+        val allHappyNums = HappyNumbers.computeAllHappyNumbers(1 to 1000)
         println(allHappyNums.mkString(", "))
-      case 1 =>
+      case Array(numStr) =>
         // Test if given number is happy
-        try {
-          val num = args.head.toInt
-          val isHappy = HappyNumbers.isHappyNumber(num)
-
-          if (isHappy)
-            println(s"$num is happy")
-          else
-            println(s"$num is not happy")
-        } catch {
-          case _: NumberFormatException => println("Argument must be an integer")
+        val num = Try(numStr.toInt).getOrElse {
+          println("Argument must be an integer")
+          return
         }
+
+        val isHappy = HappyNumbers.isHappyNumber(num)
+
+        if (isHappy)
+          println(s"$num is happy")
+        else
+          println(s"$num is not happy")
       case _ =>
         println("Usage: happy-numbers [number]")
     }
--- src/main/scala/com/zoerb/happynumbers/HappyNumbers.scala
+++ src/main/scala/com/zoerb/happynumbers/HappyNumbers.scala
@@ -2,8 +2,9 @@ package com.zoerb.happynumbers
 
 
 object HappyNumbers {
-  def computeAllHappyNumbers(min: Int, max: Int): Seq[Int] = {
-    Range.inclusive(min, max).filter(isHappyNumber)
+
+  def computeAllHappyNumbers(range: Range): Seq[Int] = {
+    range.filter(isHappyNumber)
   }
 
   def isHappyNumber(i: Int): Boolean = {
@@ -25,15 +26,14 @@ object HappyNumbers {
     sumOfSquares(splitIntoDigits(i))
   }
 
-  // Easy to update for splitting by bases other than 10
   def splitIntoDigits(i: Int): Seq[Int] = {
     @annotation.tailrec
-    def recur(i: Int, digits: Seq[Int]): Seq[Int] = {
+    def recur(i: Int, digits: List[Int]): List[Int] = {
       if (i == 0) digits
       else recur(i / 10, i % 10 +: digits)
     }
 
-    recur(i, Seq())
+    recur(i, List())
   }
 
   def sumOfSquares(nums: Seq[Int]): Int = {
@@ -45,17 +45,33 @@ object HappyNumbers {
 
 
 object HappyNumbersAlts {
-  import HappyNumbers.sq
-
   // Concise, but conceptually impure, doesn't work with negatives, dependent on toString
-  // output (e.g. what if a version of toString separated digit triples by commas)
+  // output (e.g. what if a version of toString separated digit triples by commas).  Also
+  // - admittedly getting increasingly pedantic here - it's limited to base 10 numbers.
   def splitIntoDigits_toString(i: Int): Seq[Int] = {
     require(i >= 0)
     i.toString.map(_.asDigit)
   }
 
-  // Less clear, but iterates over seq once instead of twice
-  def sumOfSquares(nums: Seq[Int]): Int = {
+  def splitIntoDigits_iterative(i: Int): Seq[Int] = {
+    import scala.collection.mutable.ListBuffer
+
+    var num = i
+    val digits = ListBuffer[Int]()
+
+    while (num != 0) {
+      digits.prepend(num % 10)
+      num /= 10
+    }
+
+    digits
+  }
+
+  // Less clear, but iterates over seq once instead of twice.  Could otherwise
+  // use a view but it'd be overkill for this use case.
+  def sumOfSquares_fold(nums: Seq[Int]): Int = {
+    import HappyNumbers.sq
+
     nums.foldLeft(0)(_ + sq(_))
   }
 }
